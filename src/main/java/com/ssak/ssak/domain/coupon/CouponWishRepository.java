c3159f7a1cadc392f6717563be75131fdc3433c4
m2nsp <mini071663@gmail.com>
2026-05-26 10:50:32 +0900

    fix: fetch join 적용으로 N+1 문제 해결

--- src/main/java/com/ssak/ssak/domain/coupon/CouponWishRepository.java
+++ src/main/java/com/ssak/ssak/domain/coupon/CouponWishRepository.java
@@ -2,6 +2,8 @@ package com.ssak.ssak.domain.coupon;
 
 import com.ssak.ssak.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +17,10 @@ public interface CouponWishRepository extends JpaRepository<CouponWish, Long> {
      * @param userId
      * @return
      */
-    List<CouponWish> findAllByUser_UserId(Long userId);
+    @Query("SELECT cw FROM CouponWish cw " +
+           "JOIN FETCH cw.coupon " +
+           "WHERE cw.user.userId = :userId")
+    List<CouponWish> findAllByUser_UserId(@Param("userId") Long userId);
 
     /**
      * 특정 사용자의 특정 쿠폰 찜 여부를 확인하고, 존재할 경우 찜 상세 내역을 반환한다.
